b7a6500c22fe43ce7b2b2c9aacb7ad74a4272602
SychO9 <sychocouldy@gmail.com>
2021-08-02 11:58:06 +0100

    Meta

--- src/Query/OnlineGambitFilter.php
+++ src/Query/OnlineGambitFilter.php
@@ -1,10 +1,12 @@
 <?php
 
 /*
- * This file is part of Afrux Online Users Widget.
+ * This file is part of afrux/online-users-widget.
  *
- * For detailed copyright and license information, please view the
- * LICENSE file that was distributed with this source code.
+ * Copyright (c) 2021 Sami Mazouz.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
  */
 
 namespace Afrux\OnlineUsers\Query;
